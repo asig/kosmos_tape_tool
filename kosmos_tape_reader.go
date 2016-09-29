b761c06dc0a672c36d29cbcd10ce731a78f91ce3
Andreas Signer <asigner@gmail.com>
2016-09-29 23:15:35 +0200

    more stuff

--- kosmos_tape_reader.go
+++ kosmos_tape_reader.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"encoding/binary"
 	"fmt"
 	"log"
-	"math"
 	"os"
 	"path/filepath"
 )
@@ -19,33 +17,6 @@ func usage() {
 	os.Exit(1)
 }
 
-func checkErr(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
-
-func check(condition bool, msg string) {
-	if !condition {
-		log.Printf(msg)
-		os.Exit(1)
-	}
-}
-
-func readUint32(f *os.File) uint32 {
-	buf := make([]byte, 4)
-	_, err := f.Read(buf)
-	checkErr(err)
-	return binary.LittleEndian.Uint32(buf)
-}
-
-func readUint16(f *os.File) uint16 {
-	buf := make([]byte, 2)
-	_, err := f.Read(buf)
-	checkErr(err)
-	return binary.LittleEndian.Uint16(buf)
-}
-
 func init() {
 	if len(os.Args) != 2 {
 		usage()
@@ -53,81 +24,52 @@ func init() {
 	filename = os.Args[1]
 }
 
-func normalize(val uint32) uint32 {
-	// The thresholds are only correct for 44100 sample rate!
-	if val < 35 {
-		return 20
-	} else {
-		return 50
+type converterState int
+
+const ( // iota is reset to 0
+	before_sync_tone converterState = iota
+	in_data          converterState = iota
+	after_data                      = iota
+)
+
+// func convertToBits(input chan Tone, output chan int) {
+
+// 	state := before_sync_tone
+
+// 	for {
+// 		tone := <-input
+// 		if tone.Duration == 0 {
+// 			break
+// 		}
+// 		switch(state) {
+// 		case before_sync_tone:
+// 			if tone.Freq == low && tone.Duration > 15 {
+// 				state = in_data
+// 			}
+// 			break
+// 		case in_data:
+// 			if tone.Freq =
+// 			case
+// 		}
+// 		log.Printf("CONSUMER: freq = %d, duration = %f", tone.Freq, tone.Duration)
+// 	}
+
+// }
+
+func readBits(toneChannel chan Tone) {
+	for {
+		tone := <-toneChannel
+		if tone.Duration == 0 {
+			break
+		}
+		log.Printf("CONSUMER: freq = %d, duration = %f", tone.Freq, tone.Duration)
 	}
+	log.Printf("DONE")
 }
 
 func main() {
-	log.Println("Reading Kosmos Tape from " + filename)
-
-	f, err := os.Open(filename)
-	checkErr(err)
-
-	check(readUint32(f) == 0x46464952, "Not a RIFF file")
-	chunkSize := readUint32(f)
-	log.Printf("Chunksize is %d", chunkSize)
-	check(readUint32(f) == 0x45564157, "Not a WAVE file")
-
-	check(readUint32(f) == 0x20746d66, "Not a 'fmt ' chunk")
-	subchunkSize := readUint32(f)
-	check(subchunkSize == 16, fmt.Sprintf("fmt Subchunk size is %d", subchunkSize))
-	audioFormat := readUint16(f)
-	numChannels := readUint16(f)
-	sampleRate := readUint32(f)
-	byteRate := readUint32(f)
-	blockAlign := readUint16(f)
-	bitsPerSample := readUint16(f)
-	check(bitsPerSample == 16, "bitsPerSample is not 16")
-
-	log.Printf("audioFormat   = %d", audioFormat)
-	log.Printf("numChannels   = %d", numChannels)
-	log.Printf("sampleRate    = %d", sampleRate)
-	log.Printf("byteRate      = %d", byteRate)
-	log.Printf("blockAlign    = %d", blockAlign)
-	log.Printf("bitsPerSample = %d", bitsPerSample)
-
-	check(readUint32(f) == 0x61746164, "Not a 'data' chunk")
-	subchunkSize = readUint32(f)
-
-	// Read data, drop everything but the first channel
-	log.Print("Loading data")
-	numSamples := subchunkSize / uint32(numChannels) / uint32(bitsPerSample/8)
-	data := make([]int16, numSamples)
-	var i uint32
-	for i = 0; i < numSamples; i++ {
-		data[i] = int16(readUint16(f))
-		f.Seek(int64((numChannels-1)*(bitsPerSample/8)), 1) // Skip other channels
-	}
-	log.Print("Data loaded")
-
-	var lastTransition uint32 = 0
-	var lastΔt uint32 = 0
-	var startPos uint32 = 0
-	for i = 0; i < numSamples-1; i++ {
-		// find zero transitions
-		if data[i] >= 0 && data[i-1] < 0 {
-			// compute time since last zero transition, normalizing values
-			Δt := normalize(i - lastTransition)
-			if math.Abs(float64(int64(Δt)-int64(lastΔt))) > 5 {
-				// frequency changed. Figure out how long we kept the same frequency.
-				l := i - startPos
-				log.Printf("Δt = %d for %d cycles (%.4f secs)", lastΔt, l, float64(l)/float64(sampleRate))
-				lastΔt = Δt
-				startPos = i
-			}
-			lastTransition = i
-		}
-	}
-	l := i - startPos
-	log.Printf("Δt = %d for %d cycles (%.4f secs)", lastΔt, l, float64(l)/float64(sampleRate))
-
-	log.Printf("numSamples    = %d", numSamples)
-
-	f.Close()
-
+	toneChannel := make(chan Tone)
+	wavReader := NewWavReader(filename, toneChannel)
+	go wavReader.read()
+	go readBits(toneChannel)
 }
---
+++ wav_reader.go
@@ -0,0 +1,156 @@
+package main
+
+import (
+	"encoding/binary"
+	"fmt"
+	"log"
+	"math"
+	"os"
+)
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func check(condition bool, msg string) {
+	if !condition {
+		log.Printf(msg)
+		os.Exit(1)
+	}
+}
+
+func readUint32(f *os.File) uint32 {
+	buf := make([]byte, 4)
+	_, err := f.Read(buf)
+	checkErr(err)
+	return binary.LittleEndian.Uint32(buf)
+}
+
+func readUint16(f *os.File) uint16 {
+	buf := make([]byte, 2)
+	_, err := f.Read(buf)
+	checkErr(err)
+	return binary.LittleEndian.Uint16(buf)
+}
+
+func normalize(val uint32) uint32 {
+	// The thresholds are only correct for 44100 sample rate!
+	if val < 35 {
+		return 20
+	} else {
+		return 50
+	}
+}
+
+type Frequency int
+
+const (
+	low  Frequency = iota
+	high Frequency = iota
+)
+
+type Tone struct {
+	Freq     Frequency
+	Duration float64
+}
+
+type WavReader struct {
+	audioFormat   uint16
+	numChannels   uint16
+	sampleRate    uint32
+	byteRate      uint32
+	blockAlign    uint16
+	bitsPerSample uint16
+
+	data    []int16
+	channel chan Tone
+}
+
+func NewWavReader(filename string, channel chan Tone) *WavReader {
+	reader := WavReader{}
+	reader.init(filename)
+	reader.channel = channel
+	return &reader
+}
+
+func (self *WavReader) init(filename string) {
+	log.Println("Reading Kosmos Tape from " + filename)
+
+	f, err := os.Open(filename)
+	checkErr(err)
+
+	check(readUint32(f) == 0x46464952, "Not a RIFF file")
+	chunkSize := readUint32(f)
+	log.Printf("Chunksize is %d", chunkSize)
+	check(readUint32(f) == 0x45564157, "Not a WAVE file")
+
+	check(readUint32(f) == 0x20746d66, "Not a 'fmt ' chunk")
+	subchunkSize := readUint32(f)
+	check(subchunkSize == 16, fmt.Sprintf("fmt Subchunk size is %d", subchunkSize))
+	self.audioFormat = readUint16(f)
+	self.numChannels = readUint16(f)
+	self.sampleRate = readUint32(f)
+	self.byteRate = readUint32(f)
+	self.blockAlign = readUint16(f)
+	self.bitsPerSample = readUint16(f)
+	check(self.bitsPerSample == 16, "bitsPerSample is not 16")
+
+	log.Printf("audioFormat   = %d", self.audioFormat)
+	log.Printf("numChannels   = %d", self.numChannels)
+	log.Printf("sampleRate    = %d", self.sampleRate)
+	log.Printf("byteRate      = %d", self.byteRate)
+	log.Printf("blockAlign    = %d", self.blockAlign)
+	log.Printf("bitsPerSample = %d", self.bitsPerSample)
+
+	check(readUint32(f) == 0x61746164, "Not a 'data' chunk")
+	subchunkSize = readUint32(f)
+
+	// Read data, drop everything but the first channel
+	log.Print("Loading data")
+	numSamples := subchunkSize / uint32(self.numChannels) / uint32(self.bitsPerSample/8)
+	self.data = make([]int16, numSamples)
+	var i uint32
+	for i = 0; i < numSamples; i++ {
+		self.data[i] = int16(readUint16(f))
+		f.Seek(int64((self.numChannels-1)*(self.bitsPerSample/8)), 1) // Skip other channels
+	}
+	log.Print("Data loaded")
+	f.Close()
+}
+
+func (self *WavReader) emit(Δt, duration uint32) {
+	var freq Frequency
+	if Δt < 30 {
+		freq = high
+	} else {
+		freq = low
+	}
+
+	log.Printf("Δt = %d for %d cycles (%.4f secs)", Δt, duration, float64(duration)/float64(self.sampleRate))
+	self.channel <- Tone{freq, float64(duration) / float64(self.sampleRate)}
+}
+
+func (self *WavReader) read() {
+	var lastTransition uint32 = 0
+	var lastΔt uint32 = 0
+	var startPos uint32 = 0
+	var i uint32
+	for i = 0; i < uint32(len(self.data))-1; i++ {
+		// find zero transitions
+		if self.data[i] >= 0 && self.data[i-1] < 0 {
+			// compute time since last zero transition, normalizing values
+			Δt := normalize(i - lastTransition)
+			if math.Abs(float64(int64(Δt)-int64(lastΔt))) > 5 {
+				// frequency changed. Figure out how long we kept the same frequency.
+				self.emit(lastΔt, i-startPos)
+				lastΔt = Δt
+				startPos = i
+			}
+			lastTransition = i
+		}
+	}
+	self.emit(lastΔt, i-startPos)
+	self.emit(0, 0)
+}
